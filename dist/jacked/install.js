1acc912fd07d27d1bb2d3ba20d2bdec9091f127d
Sairen Christian Buerano <sairen.buerano@carbonetes.com>
2023-07-10 14:38:07 +0800

    chore: add generated js files

---
+++ dist/jacked/install.js
@@ -0,0 +1,47 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.installJacked = void 0;
+const https = require("https");
+const fs = require("fs");
+const core = require("@actions/core");
+const exec = require("@actions/exec");
+// File Import
+const compile_1 = require("./compile");
+const inputs_1 = require("./inputs");
+// Class
+const userInputs = new inputs_1.Inputs();
+async function installJacked() {
+    try {
+        const options = {
+            hostname: 'raw.githubusercontent.com',
+            path: '/carbonetes/jacked/main/install.sh',
+            method: 'GET'
+        };
+        const request = https.request(options, response => {
+            let script = '';
+            response.on('data', chunk => {
+                script += chunk;
+            });
+            response.on('end', async () => {
+                await fs.promises.writeFile('./install.sh', script);
+                await exec.exec('chmod', ['+x', './install.sh']);
+                await exec.exec('./install.sh');
+                core.info('Jacked has been installed');
+                await (0, compile_1.constructArguments)(userInputs.scanType);
+            });
+        });
+        request.on('error', error => {
+            core.setFailed(error.message);
+        });
+        request.end();
+    }
+    catch (error) {
+        if (error instanceof Error) {
+            core.setFailed(error.message);
+        }
+        else {
+            core.setFailed(String(error));
+        }
+    }
+}
+exports.installJacked = installJacked;
---
+++ dist/main.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// File Import
+const install_1 = require("./jacked/install");
+(0, install_1.installJacked)();
