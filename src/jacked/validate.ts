42f0075fdbb98a72dcdd7a74dccda5a914958851
Sairen Christian Buerano <sairen.buerano@carbonetes.com>
2023-07-17 10:00:47 +0800

    fix: remove unused class

--- src/jacked/validate.ts
+++ src/jacked/validate.ts
@@ -1,16 +1,14 @@
 
 // File Import
-import { Inputs } from './inputs';
 import { Keywords } from './util/keywords';
 
 // Class
-const userInputs = new Inputs();
 const keywords = new Keywords();
 
 
 // Check if user input failCriteria is valid severity
 export function checkSeverityInput(failCriteria: string): void {
-    if (userInputs.failCriteria !== "") {
+    if (failCriteria !== "") {
         if (keywords.SEVERITY_TYPE.includes(failCriteria)) {
             console.log("InputSeverity exists in SEVERITY_TYPE");
         } else {
