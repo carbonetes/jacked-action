7fcffe5f1708b00a9c5486460f89c57be2b61751
Sairen Christian Buerano <sairen.buerano@carbonetes.com>
2023-07-17 10:01:34 +0800

    chore: update converted validate

--- dist/jacked/validate.js
+++ dist/jacked/validate.js
@@ -2,14 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.checkSeverityInput = void 0;
 // File Import
-const inputs_1 = require("./inputs");
 const keywords_1 = require("./util/keywords");
 // Class
-const userInputs = new inputs_1.Inputs();
 const keywords = new keywords_1.Keywords();
 // Check if user input failCriteria is valid severity
 function checkSeverityInput(failCriteria) {
-    if (userInputs.failCriteria !== "") {
+    if (failCriteria !== "") {
         if (keywords.SEVERITY_TYPE.includes(failCriteria)) {
             console.log("InputSeverity exists in SEVERITY_TYPE");
         }
